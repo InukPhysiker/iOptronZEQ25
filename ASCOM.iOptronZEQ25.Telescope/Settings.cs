672a6ef2cbdaaced0ccd6daadb13edd24c4c4779
InukPhysiker <InukPhysiker@gmail.com>
2020-12-28 17:02:48 -0600

    Add DeviceId and SettingsProvider attributes

--- ASCOM.iOptronZEQ25.Telescope/Settings.cs
+++ ASCOM.iOptronZEQ25.Telescope/Settings.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASCOM.iOptronZEQ25
 {
+    [DeviceId("ASCOM.iOptronZEQ25.Telescope", DeviceName = "iOptron ZEQ25 Telescope")]
+    [SettingsProvider(typeof(ASCOM.SettingsProvider))]
     internal partial class Settings
     {
     }
