e683e4bfb82488c49ea73c70a3bc6d0012bf2dfa
InukPhysiker <InukPhysiker@gmail.com>
2021-01-02 11:33:09 -0600

    Add iOptronZEQ25.TelescopeInterface project

---
+++ iOptronZEQ25.TelescopeInterface/TransactionException.cs
@@ -0,0 +1,35 @@
+﻿// This file is part of the TA.ArduinoPowerController project
+// 
+// Copyright © 2016-2017 Tigra Astronomy, all rights reserved.
+// Licensed under the MIT license, see http://tigra.mit-license.org/
+// 
+// File: TransactionException.cs  Last modified: 2017-03-16@23:33 by Tim Long
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TA.Ascom.ReactiveCommunications;
+
+namespace iOptronZEQ25.TelescopeInterface
+{
+    [Serializable]
+    public sealed class TransactionException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public TransactionException() {}
+
+        public TransactionException(string message) : base(message) {}
+
+        public TransactionException(string message, Exception inner) : base(message, inner) {}
+
+        public DeviceTransaction Transaction { get; set; }
+    }
+}
