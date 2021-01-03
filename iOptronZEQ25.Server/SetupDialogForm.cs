4e200b84faac778035e941ff651bc1f0478aa1a8
InukPhysiker <InukPhysiker@gmail.com>
2021-01-03 14:21:24 -0600

    Add SetupDialogForm

---
+++ iOptronZEQ25.Server/SetupDialogForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASCOM.iOptronZEQ25.Server
+{
+    [ComVisible(false)] // Form not registered for COM!
+    public partial class SetupDialogForm : Form
+    {
+        public SetupDialogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutBox_Click(object sender, EventArgs e)
+            {
+            //using (var aboutBox = new AboutBox())
+            //    {
+            //    aboutBox.ShowDialog();
+            //    }
+            }
+
+        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
+            {
+            try
+                {
+                Process.Start("http://ascom-standards.org/");
+                }
+            catch (Win32Exception noBrowser)
+                {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+                }
+            catch (Exception other)
+                {
+                MessageBox.Show(other.Message);
+                }
+            }
+
+        private void cmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
+            {
+            Close();
+            }
+
+        private void cmdOK_Click(object sender, EventArgs e) // OK button event handler
+            {
+            communicationSettingsControl1.Save();
+            }
+
+        private void SetupDialogForm_Load(object sender, EventArgs e)
+            {
+            var onlineClients = SharedResources.ConnectionManager.OnlineClientCount;
+            if (onlineClients == 0)
+                {
+                communicationSettingsControl1.Enabled = true;
+                ConnectionErrorProvider.SetError(communicationSettingsControl1, string.Empty);
+                }
+            else
+                {
+                communicationSettingsControl1.Enabled = false;
+                ConnectionErrorProvider.SetError(communicationSettingsControl1,
+                    "Connection settings cannot be changed while there are connected clients");
+                }
+            }
+    }
+}
