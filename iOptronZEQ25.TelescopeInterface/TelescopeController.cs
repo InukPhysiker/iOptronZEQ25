7b028ee9363a0b3aab90310560493a86ef43e511
InukPhysiker <InukPhysiker@gmail.com>
2021-01-03 15:37:05 -0600

    remove and sort usings

--- iOptronZEQ25.TelescopeInterface/TelescopeController.cs
+++ iOptronZEQ25.TelescopeInterface/TelescopeController.cs
@@ -1,11 +1,6 @@
-﻿using NLog;
+using NLog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TA.Ascom.ReactiveCommunications;
-using TA.Ascom.ReactiveCommunications.Transactions;
 
 namespace iOptronZEQ25.TelescopeInterface
 {
