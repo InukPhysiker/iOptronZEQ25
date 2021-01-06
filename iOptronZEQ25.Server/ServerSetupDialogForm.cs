a6ea04af43cc46dfd3fd67bdb326043cca44c43e
InukPhysiker <InukPhysiker@gmail.com>
2021-01-05 18:38:26 -0600

    update to ServerSetupDialogForm_Load and format

--- iOptronZEQ25.Server/ServerSetupDialogForm.cs
+++ iOptronZEQ25.Server/ServerSetupDialogForm.cs
@@ -12,63 +12,65 @@ using System.Windows.Forms;
 
 namespace ASCOM.iOptronZEQ25.Server
 {
-    [ComVisible(false)] // Form not registered for COM!
-    public partial class ServerSetupDialogForm : Form
-    {
-        public ServerSetupDialogForm()
-        {
-            InitializeComponent();
-        }
+	[ComVisible(false)] // Form not registered for COM!
+	public partial class ServerSetupDialogForm : Form
+	{
+		public ServerSetupDialogForm()
+		{
+			InitializeComponent();
+		}
 
-        private void AboutBox_Click(object sender, EventArgs e)
-            {
-            //using (var aboutBox = new AboutBox())
-            //    {
-            //    aboutBox.ShowDialog();
-            //    }
-            }
+		private void AboutBox_Click(object sender, EventArgs e)
+		{
+			//using (var aboutBox = new AboutBox())
+			//{
+			//	aboutBox.ShowDialog();
+			//}
+		}
 
-        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
-            {
-            try
-                {
-                Process.Start("http://ascom-standards.org/");
-                }
-            catch (Win32Exception noBrowser)
-                {
-                if (noBrowser.ErrorCode == -2147467259)
-                    MessageBox.Show(noBrowser.Message);
-                }
-            catch (Exception other)
-                {
-                MessageBox.Show(other.Message);
-                }
-            }
+		private void BrowseToAscom(object sender, EventArgs e)
+		{
+			try
+			{
+				Process.Start("http://ascom-standards.org/");
+			}
+			catch (Win32Exception noBrowser)
+			{
+				if (noBrowser.ErrorCode == -2147467259)
+					MessageBox.Show(noBrowser.Message);
+			}
+			catch (Exception other)
+			{
+				MessageBox.Show(other.Message);
+			}
+		}
 
-        private void cmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
-            {
-            Close();
-            }
 
-        private void cmdOK_Click(object sender, EventArgs e) // OK button event handler
-            {
-            communicationSettingsControl1.Save();
-            }
+		private void cmdCancel_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
 
-        private void SetupDialogForm_Load(object sender, EventArgs e)
-            {
-            var onlineClients = SharedResources.ConnectionManager.OnlineClientCount;
-            if (onlineClients == 0)
-                {
-                communicationSettingsControl1.Enabled = true;
-                ConnectionErrorProvider.SetError(communicationSettingsControl1, string.Empty);
-                }
-            else
-                {
-                communicationSettingsControl1.Enabled = false;
-                ConnectionErrorProvider.SetError(communicationSettingsControl1,
-                    "Connection settings cannot be changed while there are connected clients");
-                }
-            }
-    }
+		private void cmdOK_Click(object sender, EventArgs e)
+		{
+			communicationSettingsControl1.Save();
+		}
+
+
+		private void ServerSetupDialogForm_Load(object sender, EventArgs e)
+		{
+			var onlineClients = SharedResources.ConnectionManager.OnlineClientCount;
+			if (onlineClients == 0)
+			{
+				communicationSettingsControl1.Enabled = true;
+				ConnectionErrorProvider.SetError(communicationSettingsControl1, string.Empty);
+			}
+			else
+			{
+				communicationSettingsControl1.Enabled = false;
+				ConnectionErrorProvider.SetError(communicationSettingsControl1,
+					"Connection settings cannot be changed while there are connected clients");
+			}
+		}
+	}
 }
