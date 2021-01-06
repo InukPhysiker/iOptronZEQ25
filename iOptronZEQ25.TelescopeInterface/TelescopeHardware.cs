9bca7402ed0cc7b2b64dff554ff230db9e0b9d66
InukPhysiker <InukPhysiker@gmail.com>
2021-01-06 07:53:20 -0600

    Add TelescopeHardware.cs and Transactions.cs

---
+++ iOptronZEQ25.TelescopeInterface/TelescopeHardware.cs
@@ -0,0 +1,270 @@
+﻿using ASCOM.DeviceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TA.Ascom.ReactiveCommunications.Transactions;
+
+namespace iOptronZEQ25.TelescopeInterface
+{
+    public partial class TelescopeController : IDisposable
+    {
+        #region ITelescope Implementation
+
+        public void AbortSlew()
+        {
+            var AbortSlewTransaction = new NoReplyTransaction(":q#");
+            //    throw new ASCOM.MethodNotImplementedException("AbortSlew");
+        }
+
+        //public bool AtHome
+        //{
+        //    get
+        //    {
+        //        // Command: “:AH#”
+        //        // Respond: “0” The telescope is not at “home” position,
+        //        // “1” The telescope is at “home” position.
+        //        // This command returns whether the telescope is at “home” position.
+        //        var AtHomeTransaction = new TerminatedStringTransaction(":AH#", '#', ':') { Timeout = TimeSpan.FromSeconds(2) };
+        //        Task.Run(() => transactionProcessor.CommitTransaction(AtHomeTransaction));
+        //        AtHomeTransaction.WaitForCompletionOrTimeout();
+        //        return false;
+        //    }
+        //}
+
+        //public double Azimuth
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("Azimuth", false);
+        //    }
+        //}
+
+        //public double Declination
+        //{
+        //    get
+        //    {
+        //        double declination = 0.0;
+        //        return declination;
+        //    }
+        //}
+
+        //public double DeclinationRate
+        //{
+        //    get
+        //    {
+        //        double declination = 0.0;
+        //        return declination;
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("DeclinationRate", true);
+        //    }
+        //}
+
+        //public PierSide DestinationSideOfPier(double RightAscension, double Declination)
+        //{
+        //    throw new ASCOM.PropertyNotImplementedException("DestinationSideOfPier", false);
+        //}
+
+
+        //public void FindHome()
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("FindHome");
+        //}
+
+        //public double GuideRateDeclination
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("GuideRateDeclination", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("GuideRateDeclination", true);
+        //    }
+        //}
+
+        //public double GuideRateRightAscension
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("GuideRateRightAscension", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("GuideRateRightAscension", true);
+        //    }
+        //}
+
+        //public bool IsPulseGuiding
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("IsPulseGuiding", false);
+        //    }
+        //}
+
+        //public void MoveAxis(TelescopeAxes Axis, double Rate)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("MoveAxis");
+        //}
+
+        //public void PulseGuide(GuideDirections Direction, int Duration)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("PulseGuide");
+        //}
+
+        //public double RightAscension
+        //{
+        //    get
+        //    {
+        //        double rightAscension = 0.0;
+        //        return rightAscension;
+        //    }
+        //}
+
+        //public double RightAscensionRate
+        //{
+        //    get
+        //    {
+        //        double rightAscensionRate = 0.0;
+        //        return rightAscensionRate;
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("RightAscensionRate", true);
+        //    }
+        //}
+
+        //public PierSide SideOfPier
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("SideOfPier", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("SideOfPier", true);
+        //    }
+        //}
+
+        //public double SiteLatitude
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("SiteLatitude", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("SiteLatitude", true);
+        //    }
+        //}
+
+        //public double SiteLongitude
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("SiteLongitude", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("SiteLongitude", true);
+        //    }
+        //}
+
+        //public void SlewToAltAz(double Azimuth, double Altitude)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SlewToAltAz");
+        //}
+
+        //public void SlewToAltAzAsync(double Azimuth, double Altitude)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SlewToAltAzAsync");
+        //}
+
+        //public void SlewToCoordinates(double RightAscension, double Declination)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SlewToCoordinates");
+        //}
+
+        //public void SlewToCoordinatesAsync(double RightAscension, double Declination)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SlewToCoordinatesAsync");
+        //}
+
+        //public void SlewToTarget()
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SlewToTarget");
+        //}
+
+        //public void SlewToTargetAsync()
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SlewToTargetAsync");
+        //}
+
+        //public bool Slewing
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("Slewing", false);
+        //    }
+        //}
+
+        //public void SyncToAltAz(double Azimuth, double Altitude)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SyncToAltAz");
+        //}
+
+        //public void SyncToCoordinates(double RightAscension, double Declination)
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SyncToCoordinates");
+        //}
+
+        //public void SyncToTarget()
+        //{
+        //    throw new ASCOM.MethodNotImplementedException("SyncToTarget");
+        //}
+
+        //public double TargetDeclination
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("TargetDeclination", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("TargetDeclination", true);
+        //    }
+        //}
+
+        //public double TargetRightAscension
+        //{
+        //    get
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("TargetRightAscension", false);
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("TargetRightAscension", true);
+        //    }
+        //}
+
+        //public bool Tracking
+        //{
+        //    get
+        //    {
+        //        bool tracking = true;
+        //        return tracking;
+        //    }
+        //    set
+        //    {
+        //        //        throw new ASCOM.PropertyNotImplementedException("Tracking", true);
+        //    }
+        //}
+
+        #endregion
+
+    }
+}
---
+++ iOptronZEQ25.TelescopeInterface/Transactions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TA.Ascom.ReactiveCommunications;
+
+namespace iOptronZEQ25.TelescopeInterface
+{
+    /// <summary>
+    ///     Receives a response consisting of <c>0#</c> or <c>1#</c> and interprets it as a boolean.
+    /// </summary>
+    public class ZEQ25BooleanTransaction : DeviceTransaction
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DeviceTransaction" /> class.
+        /// </summary>
+        /// <param name="command">The command string to send to the device.</param>
+        public ZEQ25BooleanTransaction(string command) : base(command)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(command));
+        }
+
+        /// <summary>
+        ///     Observes the character sequence from the communications channel
+        ///     until a satisfactory response has been received.
+        /// </summary>
+        /// <param name="source">The source sequence.</param>
+        public override void ObserveResponse(IObservable<char> source)
+        {
+            //source.TerminatedBoolean('#').Take(1).Subscribe(OnNext, OnError, OnCompleted);
+            source.TerminatedBoolean('#').Take(1).Subscribe(OnNext, OnError, OnCompleted);
+            //source.Take(1).Subscribe(OnNext, OnError, OnCompleted);
+        }
+
+        /// <summary>
+        ///     Called when the response sequence completes. This indicates a successful transaction.
+        /// </summary>
+        /// <remarks>
+        ///     If there has been a valid response (<c>Response.Any() == true</c>) then it is converted to a boolean and placed in
+        ///     the <see cref="Value" /> property.
+        /// </remarks>
+        protected override void OnCompleted()
+        {
+            try
+            {
+                if (Response.Any())
+                    Value = Response.Single().StartsWith("1");
+            }
+            catch (FormatException)
+            {
+                Value = false;
+            }
+            base.OnCompleted();
+        }
+
+        /// <summary>
+        ///     Gets the final value of teh transaction's response, as a boolean.
+        /// </summary>
+        /// <value><c>true</c> if value; otherwise, <c>false</c>.</value>
+        public bool Value { get; private set; }
+    }
+}
