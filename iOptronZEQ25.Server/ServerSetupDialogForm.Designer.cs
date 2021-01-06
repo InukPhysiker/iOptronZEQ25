d43412ebd64a3ff29d80754507d7e7caf4baae46
InukPhysiker <InukPhysiker@gmail.com>
2021-01-05 18:44:57 -0600

    Update Tab Indices

--- iOptronZEQ25.Server/ServerSetupDialogForm.Designer.cs
+++ iOptronZEQ25.Server/ServerSetupDialogForm.Designer.cs
@@ -1,4 +1,4 @@
-
+﻿
 namespace ASCOM.iOptronZEQ25.Server
 {
     partial class ServerSetupDialogForm
@@ -33,9 +33,9 @@ namespace ASCOM.iOptronZEQ25.Server
             this.cmdOK = new System.Windows.Forms.Button();
             this.cmdCancel = new System.Windows.Forms.Button();
             this.picASCOM = new System.Windows.Forms.PictureBox();
-            this.communicationSettingsControl1 = new CommunicationSettingsControl();
-            this.AboutBox = new System.Windows.Forms.Button();
             this.ConnectionErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.AboutBox = new System.Windows.Forms.Button();
+            this.communicationSettingsControl1 = new ASCOM.iOptronZEQ25.Server.CommunicationSettingsControl();
             ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ConnectionErrorProvider)).BeginInit();
             this.SuspendLayout();
@@ -47,7 +47,7 @@ namespace ASCOM.iOptronZEQ25.Server
             this.cmdOK.Location = new System.Drawing.Point(12, 139);
             this.cmdOK.Name = "cmdOK";
             this.cmdOK.Size = new System.Drawing.Size(59, 24);
-            this.cmdOK.TabIndex = 0;
+            this.cmdOK.TabIndex = 1;
             this.cmdOK.Text = "OK";
             this.cmdOK.UseVisualStyleBackColor = true;
             this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
@@ -59,7 +59,7 @@ namespace ASCOM.iOptronZEQ25.Server
             this.cmdCancel.Location = new System.Drawing.Point(77, 139);
             this.cmdCancel.Name = "cmdCancel";
             this.cmdCancel.Size = new System.Drawing.Size(59, 25);
-            this.cmdCancel.TabIndex = 1;
+            this.cmdCancel.TabIndex = 2;
             this.cmdCancel.Text = "Cancel";
             this.cmdCancel.UseVisualStyleBackColor = true;
             this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
@@ -69,6 +69,7 @@ namespace ASCOM.iOptronZEQ25.Server
             this.picASCOM.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.picASCOM.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picASCOM.Image = global::ASCOM.iOptronZEQ25.Server.Properties.Resources.ASCOM;
+            this.picASCOM.InitialImage = global::ASCOM.iOptronZEQ25.Server.Properties.Resources.ASCOM;
             this.picASCOM.Location = new System.Drawing.Point(292, 9);
             this.picASCOM.Name = "picASCOM";
             this.picASCOM.Size = new System.Drawing.Size(48, 56);
@@ -76,25 +77,6 @@ namespace ASCOM.iOptronZEQ25.Server
             this.picASCOM.TabIndex = 3;
             this.picASCOM.TabStop = false;
             this.picASCOM.Click += new System.EventHandler(this.BrowseToAscom);
-            this.picASCOM.DoubleClick += new System.EventHandler(this.BrowseToAscom);
-            // 
-            // communicationSettingsControl1
-            // 
-            this.ConnectionErrorProvider.SetIconAlignment(this.communicationSettingsControl1, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
-            this.communicationSettingsControl1.Location = new System.Drawing.Point(12, 43);
-            this.communicationSettingsControl1.Name = "communicationSettingsControl1";
-            this.communicationSettingsControl1.Size = new System.Drawing.Size(238, 22);
-            this.communicationSettingsControl1.TabIndex = 7;
-            // 
-            // AboutBox
-            // 
-            this.AboutBox.Location = new System.Drawing.Point(263, 139);
-            this.AboutBox.Name = "AboutBox";
-            this.AboutBox.Size = new System.Drawing.Size(75, 23);
-            this.AboutBox.TabIndex = 8;
-            this.AboutBox.Text = "About...";
-            this.AboutBox.UseVisualStyleBackColor = true;
-            this.AboutBox.Click += new System.EventHandler(this.AboutBox_Click);
             // 
             // ConnectionErrorProvider
             // 
@@ -102,21 +84,36 @@ namespace ASCOM.iOptronZEQ25.Server
             this.ConnectionErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
             this.ConnectionErrorProvider.ContainerControl = this;
             // 
-            // SetupDialogForm
+            // AboutBox
+            // 
+            this.AboutBox.Location = new System.Drawing.Point(263, 139);
+            this.AboutBox.Name = "AboutBox";
+            this.AboutBox.Size = new System.Drawing.Size(75, 23);
+            this.AboutBox.TabIndex = 4;
+            this.AboutBox.Text = "About...";
+            this.AboutBox.UseVisualStyleBackColor = true;
+            this.AboutBox.Click += new System.EventHandler(this.AboutBox_Click);
+            // 
+            // communicationSettingsControl1
+            // 
+            this.communicationSettingsControl1.Location = new System.Drawing.Point(13, 9);
+            this.communicationSettingsControl1.Name = "communicationSettingsControl1";
+            this.communicationSettingsControl1.Size = new System.Drawing.Size(255, 22);
+            this.communicationSettingsControl1.TabIndex = 0;
+            // 
+            // ServerSetupDialogForm
             // 
             this.AcceptButton = this.cmdOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cmdCancel;
             this.ClientSize = new System.Drawing.Size(350, 175);
-            this.Controls.Add(this.AboutBox);
             this.Controls.Add(this.communicationSettingsControl1);
+            this.Controls.Add(this.AboutBox);
             this.Controls.Add(this.picASCOM);
             this.Controls.Add(this.cmdCancel);
             this.Controls.Add(this.cmdOK);
-            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::ASCOM.iOptronZEQ25.Server.Properties.Settings.Default, "SetupDialogLocation", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Location = global::ASCOM.iOptronZEQ25.Server.Properties.Settings.Default.SetupDialogLocation;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ServerSetupDialogForm";
@@ -136,8 +133,8 @@ namespace ASCOM.iOptronZEQ25.Server
         private System.Windows.Forms.Button cmdOK;
         private System.Windows.Forms.Button cmdCancel;
         private System.Windows.Forms.PictureBox picASCOM;
-        private CommunicationSettingsControl communicationSettingsControl1;
-        private System.Windows.Forms.Button AboutBox;
         private System.Windows.Forms.ErrorProvider ConnectionErrorProvider;
+        private System.Windows.Forms.Button AboutBox;
+        private CommunicationSettingsControl communicationSettingsControl1;
     }
 }
