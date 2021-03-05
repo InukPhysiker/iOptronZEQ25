540852839f51c93cbf504a3c00083d0b4b1b8b7c
InukPhysiker <InukPhysiker@gmail.com>
2021-03-05 06:13:52 -0600

    set ComVisible(true)

--- iOptronZEQ25.TelescopeInterface/Properties/AssemblyInfo.cs
+++ iOptronZEQ25.TelescopeInterface/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("360efffb-1319-433d-8ed9-8ed3c1ad3c45")]
